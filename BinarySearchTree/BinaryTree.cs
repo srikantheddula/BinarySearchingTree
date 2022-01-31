14685bad2b4c12f35d67a987c4649432a23dcd26
srikantheddula <you@example.com>
2022-01-31 20:38:17 +0530

    [srikantheddula]:adding InsertValues implemented in BinarySearhTree

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{/// <summary>
+/// inserting values by BinarySearchTable
+/// </summary>
+/// <typeparam name="T"></typeparam>
+    class BinaryTree<T> where T : IComparable
+    {
+        public T NodeData { get; set; }
+        public BinaryTree<T> LeftTree { get; set; }
+        public BinaryTree<T> RightTree { get; set; }
+        public BinaryTree(T nodeData) // creating the constructer to intialise the vlaues
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item) // this method is used fot inserting the values to the binary tree
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinaryTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinaryTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display() // this method is used for the to display the vlaues un the tree
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -2,11 +2,14 @@
 
 namespace BinarySearchTree
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to BinarySearchTree");
+            BinaryTree<int> binaryTree = new BinaryTree<int>(56); 
+            binaryTree.Insert(30);  //Inserting the values to the Binary tree
+            binaryTree.Insert(70);
+            binaryTree.Display();
         }
     }
 }
